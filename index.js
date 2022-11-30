bddc4e1eb8210c1ec39fee528ff3dbb6fe7f8632
Osauyi <victorimafidon17@gmail.com>
2022-11-30 12:02:44 +0100

    fixed the server

--- index.js
+++ index.js
@@ -1,39 +1,23 @@
-const express = require("express")
-const request = require("request")
-const https = require("https");
-const { options } = require("request");
-const { response } = require("express");
-const bodyParser = require('body-parser')
-
-const app = express()
-
-app.use(express.static('public'));
-
-app.use(bodyParser.urlencoded({ extended: false }))
-
-
-app.get("/", function(req, res){
-  res.sendFile(__dirname + "/index.html")
-})
-
-
 const url = "https://api.adviceslip.com/advice";
 
-app.post("/", function(req, res) {
+var id = 0 
+var string = ""
 
-  let lucy = req.body.game
-  request(url, function(error, response, body){
-    var data = JSON.parse(body)
+
+function dice() {
+  fetch(url)
+  .then(res => res.json())
+  .then((data) => {
     id = data.slip.id
-    lucy = id
-    console.log(lucy)
+    string = data.slip.advice
+
+    document.querySelector("h1").textContent = `Advice #${id}`
+    document.querySelector("p").textContent = `"${string}"`
+    
   })
+  console.log("sup")
+}
 
-res.sendFile(__dirname + "/index.html")
-})
 
-  
-
-app.listen(3000, function() {
-  console.log("Server running........(yeah RiGhT)")
-})   
+document.querySelector("button").addEventListener("click", dice) 
+dice()
